cc6640b9283a1293539ee29050fdd43443485226
KonovalovaDarya <konovalova.daa@gmail.com>
2024-06-27 11:34:51 +0700

    Homework4

---
+++ Numbers.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Numbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int sum = 0, sumOdd = 0, max = 0;
+
+        do {
+            int n = x % 10;
+
+            sum = sum + n; // сумма всех цифр
+
+            if (n % 2 != 0 ) {
+                sumOdd = sumOdd + n;  // сумма нечетных
+            }
+
+            if (n > max) {
+                max = n; // поиск максимального
+            }
+
+            x = x / 10;
+
+        } while (x > 0);
+
+        System.out.println("Summ of numbers = " + sum);
+        System.out.println("Summ of odd numbers = " + sumOdd);
+        System.out.println("Max number = " + max);
+    }
+}
